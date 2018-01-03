1632f95cef00710add7f390181068e759c172dc9
Caio Batista <cbatista.93@gmail.com>
2018-01-03 00:40:13 -0200

    authform styled and inside login form

---
+++ client/components/AuthForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+class AuthForm extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       email: '',
+       password: ''
+    }
+  }
+
+  onSubmitForm() {
+    e.preventDefault();
+
+    console.log('form submited');
+  }
+  
+  render() {
+    const { email, password } = this.props;
+    return (
+      <div className="row">
+        <form className="col s4" onSubmit={this.onSubmitForm.bind(this)}>
+          <div className="input-field">
+            <label>Email</label>
+            <input
+              value={email}
+              onChange={() => this.setState({ email: e.target.value })}
+            />
+          </div>
+          <div className="input-field">
+            <label>Password</label>
+            <input 
+              value={password}
+              onChange={() => this.setState({ password: e.target.value})}
+            />
+          </div>
+          <button className="btn">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AuthForm;
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
+import AuthForm from './AuthForm';
+
 class LoginForm extends Component {
   render() {
     return (
-      <div>
-        Login form
-      </div>
+      <AuthForm />
     );
   }
 }
